18b2746c215f7e30c884bf874e0561c17989c514
rangermauve <RangerMauve@hotmail.com>
2014-11-13 22:22:57 -0500

    Functional example with dynamic DOM

--- example/index.js
+++ example/index.js
@@ -1,6 +1,7 @@
 var domDelegate = require("../");
 var map = require("through2-map").obj;
-var stdout = require("stdout")();
+var concat = require("array-concat-stream");
+var patcher = require("html-patcher-stream");
 
 var output = document.getElementById("output");
 
@@ -10,4 +11,10 @@ delegate.on("click", "button")
 	.pipe(map(function (event) {
 		return event.target.innerText
 	}))
-	.pipe(stdout);
+	.pipe(concat([]))
+	.pipe(map(function (items) {
+		return "<div>" + items.map(function (item) {
+			return "<button>" + item + "</button>";
+		}).join("\n") + "</div>";
+	}))
+	.pipe(patcher(output, "<div></div>"))
